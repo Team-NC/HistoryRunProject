9a69ce16ade1b91aba411495df7f59b6a61bcc5d
doh-ha <101639449+doh-ha@users.noreply.github.com>
2023-05-17 16:58:47 +0900

    🚚 MainMenu -> StartMenu 이름 변겅

--- HistoryRunProject/Assets/StartMenu.cs
+++ HistoryRunProject/Assets/StartMenu.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class MainMenu : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
