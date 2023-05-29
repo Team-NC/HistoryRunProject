9b4b1a0cf75cfaa2a31ef00b646854647c74a267
Taeyoung Kim <lucy_ia@ewhain.net>
2023-05-29 20:57:24 +0900

    Feat: PuzzleGame

---
+++ HistoryRunProject/Assets/Scripts/PUZ_DragAndDrop.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PUZ_DragAndDrop : MonoBehaviour
+{
+    public GameObject SelectedPiece;
+
+    private int OIL = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.transform.CompareTag("PUZ"))
+            {
+                if (!hit.transform.GetComponent<PUZ_PiecesScript>().InRightPosition)
+                {
+                    SelectedPiece = hit.transform.gameObject;
+                    SelectedPiece.GetComponent<PUZ_PiecesScript>().Selected = true;
+                    SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (SelectedPiece != null)
+            {
+                SelectedPiece.GetComponent<PUZ_PiecesScript>().Selected = false;
+                SelectedPiece = null; 
+            }
+        }
+
+        if (SelectedPiece != null)
+        {
+            Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SelectedPiece.transform.position = new Vector3(MousePoint.x, MousePoint.y, 0);
+        }
+            
+    }
+}
---
+++ HistoryRunProject/Assets/Scripts/PUZ_PiecesScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PUZ_PiecesScript : MonoBehaviour
+{
+    private Vector3 RightPosition;
+
+    public bool InRightPosition;
+
+    public bool Selected;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        RightPosition = transform.position;
+        transform.position = new Vector3(Random.Range(-4f, 4f), Random.Range(-12f, -9f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, RightPosition) < 0.5f)
+        {
+            if (!Selected)
+            {
+                if (InRightPosition == false)
+                {
+                    transform.position = RightPosition;
+                    InRightPosition = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                }
+                
+            }
+        }
+    }
+}
